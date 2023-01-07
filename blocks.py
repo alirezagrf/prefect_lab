2bb4f30c0d0010f62df1a64627dd2520b98d6895
alirezagrf <alireza@greenfact.com>
2023-01-07 16:55:16 +0100

    test capital env var name

--- blocks.py
+++ blocks.py
@@ -3,14 +3,14 @@ from prefect_aws import AwsCredentials
 import os
 from prefect.filesystems import GitHub
 
-s3_bucket = S3(
-        bucket_path='s3-bucket',
-        basepath='rend-prefect-s3',
-        aws_credentials=AwsCredentials(
-                                        aws_access_key_id=os.getenv('aws_access_key_id'),
-                                        aws_secret_access_key=os.getenv('aws_secret_access_key'))
-    )
-s3_bucket.save(name='s3-bucket', overwrite=True)
+# s3_bucket = S3(
+#         bucket_path='s3-bucket',
+#         basepath='rend-prefect-s3',
+#         aws_credentials=AwsCredentials(
+#                                         aws_access_key_id=os.getenv('aws_access_key_id'),
+#                                         aws_secret_access_key=os.getenv('aws_secret_access_key'))
+#     )
+# s3_bucket.save(name='s3-bucket', overwrite=True)
 
 github_storage = GitHub(reference='main',repository=os.getenv('GITHUB_REPOSITORY'))
 github_storage.save(name='github',overwrite=True)
